9f2d46178bd37b5c1dc7470430ad576f18cc0787
Vlad Bondarev <bondarev.21@icloud.com>
2024-05-07 13:10:12 +0300

    fixed some code in CustomGlobalExceptionHandler

--- src/main/java/api/constant/ExceptionsConstantsHolder.java
+++ src/main/java/api/constant/ExceptionsConstantsHolder.java
@@ -3,6 +3,5 @@ package api.constant;
 public interface ExceptionsConstantsHolder {
     String TIME_STAMP = "timestamp";
     String STATUS = "status";
-    String ERRORS = "errors";
     String MESSAGE = "message";
 }
